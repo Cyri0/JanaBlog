d0ba99c317af4394d51355d59bd0e257308edd20
Cyri0 <nemestamas94@gmail.com>
2020-03-17 13:22:55 +0100

    Dropdown menu solved, mobile mode solved :D

--- js/script.js
+++ js/script.js
@@ -11,11 +11,11 @@ let list = [];
 
 function init(){
     list = [
-    new Article("Jussát várja a Vaják...","pics/vajak.jpg"),
-    new Article("Fush-Ro-Dah!","pics/skyrim.jpg"),
-    new Article("GTA San Anreas - A Kritika","https://toptechytips.com/wp-content/uploads/2019/10/GTA-San-Andreas-Apk-obb.jpg"),
-    new Article("Teszt","pics/latin.jpg"),
-    new Article("Teszt","pics/latin.jpg")
+        new Article("Jussát várja a Vaják...","pics/vajak.jpg"),
+        new Article("Fush-Ro-Dah!","pics/skyrim.jpg"),
+        new Article("GTA San Andreas - A Kritika","https://toptechytips.com/wp-content/uploads/2019/10/GTA-San-Andreas-Apk-obb.jpg"),
+        new Article("Teszt","pics/latin.jpg"),
+        new Article("Teszt","pics/latin.jpg")
     ];
 
     for(let i = 0; i < list.length; i++){
@@ -44,6 +44,37 @@ function reset_animation(pic) {
   pic.style.animation = 'none';
   pic.offsetHeight;
   pic.style.animation = null; 
+}
+
+let actual_num = 0;
+
+function next_post(){
+    
+    if(actual_num != 5){
+        actual_num++;
+    } else {
+        actual_num = 0;
+    }
+    
+    change(actual_num);
+}
+
+function prev_post(){
+    if(actual_num != 0){
+        actual_num--;
+    } else {
+        actual_num = 4;
+    }
+    change(actual_num);
+}
+
+function mobile_dropdown_handler(){
+    let mobile_menu = document.getElementById("mobile_dropdown_menu");
+    if(mobile_menu.style.visibility != "visible"){
+        mobile_menu.style.visibility = "visible";}
+    else {
+        mobile_menu.style.visibility = "hidden";
+    }
 }
 
 init();
